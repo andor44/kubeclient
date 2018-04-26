52891c9521672e10aeff2470817cce7b8984bf14
Andor Uhlár <andor44@gmail.com>
2018-04-26 16:45:32 +0200

    sigh

--- src/api/authentication/v1/mod.rs
+++ src/api/authentication/v1/mod.rs
@@ -5,8 +5,12 @@ type ExtraValue = Vec<String>;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct UserInfo {
-    pub username: Option<String>,
-    pub uid: Option<String>,
+    #[serde(default, skip_serializing_if = "String::is_empty")]
+    pub username: String,
+    #[serde(default, skip_serializing_if = "String::is_empty")]
+    pub uid: String,
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
     pub groups: Vec<String>,
+    #[serde(default, skip_serializing_if = "HashMap::is_empty")]
     pub extra: HashMap<String, ExtraValue>,
 }
